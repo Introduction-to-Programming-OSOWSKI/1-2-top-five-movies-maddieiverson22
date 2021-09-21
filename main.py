2613c6757434f5d59a683efe508746bafca08d22
maddieiverson22 <825522@mystma.org>
2021-09-21 14:50:15 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
 
 def topFiveMovies(a, b, c, d, e):
-    return(a, b, c, d, e)
-topFiveMovies("Harry Potter", "One Direction Documentary", "Star Wars", "Legend of Sleepy Hallow", "Nacho Libre")
+    return a + b + c + d + e
+print("Harry Potter", "One Direction Documentary", "Star Wars", "Legend of Sleepy Hallow", "Nacho Libre")
